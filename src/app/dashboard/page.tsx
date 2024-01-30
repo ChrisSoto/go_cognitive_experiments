8a263985e5a4db839f1d576aec9e6299cbfa21c1
chris soto <christopherrsoto@gmail.com>
2024-01-30 13:09:31 -0800

    working on same basics

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,9 +1,24 @@
-import Navbar from "../ui/navbar/navbar";
+import Link from "next/link";
+import Modal from "../ui/components/Modal";
 
-export default function Page() {
+type SearchParamProps = {
+  searchParams: Record<string, string> | null | undefined;
+}
+
+export default function Page({ searchParams }: SearchParamProps) {
+
+  const show = searchParams?.show;
+
   return (
     <>
-      <p>dashboard</p>
+      <h2>Page Title</h2>
+      <section>dashboard</section>
+
+      <Link href={"dashboard/?show=true"}>
+        Summon Modal
+      </Link>
+
+      { show && <Modal returnRoute={"dashboard"}/> }
     </>
   );
 }
