256e5aabae7bcc00134ac4032cbbeb94cecfd45d
chris soto <christopherrsoto@gmail.com>
2024-01-30 20:50:01 -0800

    added modal

--- src/app/ui/components/Modal.tsx
+++ src/app/ui/components/Modal.tsx
@@ -5,7 +5,6 @@ type ReturnRouteProps = {
 }
 
 export default function Modal({ returnRoute }: ReturnRouteProps) {
-
   return (
     <div className="fixed inset-0 bg-gray-600 bg-opacity-50 overflow-y-auto h-full w-full flex items-center justify-center">
       <div className="p-8 border w-96 shadow-lg rounded-md bg-white">
