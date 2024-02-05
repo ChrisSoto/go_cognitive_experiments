90ad483b5042ad02446861106806648213c3d10d
chris soto <christopherrsoto@gmail.com>
2024-02-05 13:14:11 -0800

    update layout

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -10,15 +10,32 @@ export default function Page({ searchParams }: SearchParamProps) {
   const show = searchParams?.show;
 
   return (
-    <>
-      <h2>Page Title</h2>
-      <section>dashboard</section>
+    <> 
+      <div className="h-full">
+        {/* <h1 className="text-2xl font-bold">Page Title</h1> */}
+        <div className="flex flex-col gap-5 h-full">
 
-      <Link href={"dashboard/?show=true"}>
+          <section className="flex">
+            <h1 className="text-5xl">Dashboard</h1>
+          </section>
+
+          <div className="flex flex-row gap-5 h-full">
+            <div className="flex flex-col basis-1/2 gap-5">
+              <section className="bg-green-200 h-1/4">Activity Chart</section>
+              <section className="bg-green-400 h-3/4">Comments</section>
+            </div>
+            
+            <section className="bg-green-600 basis-1/2">List of Experiments</section>
+          </div>
+
+        </div>
+      </div>
+
+      {/* <Link href={"dashboard/?show=true"}>
         Summon Modal
       </Link>
 
-      { show && <Modal returnRoute={"dashboard"}/> }
+      { show && <Modal returnRoute={"dashboard"}/> } */}
     </>
   );
 }
